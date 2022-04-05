341fe08fa102684c1d26cb710f6d0afb2270038e
robweber <robweberjr@gmail.com>
2022-04-05 15:49:06 -0500

    update files for Python3 don't need __init_ anymore

--- default.py
+++ default.py
@@ -8,14 +8,14 @@ class Toggle:
     REMOTE_MODE = 0
     LOCAL_MODE = 1
     UNKNOWN_MODE = 2
-    
-    fileLoc = xbmc.translatePath(utils.data_dir() + "mode.json")
+
+    fileLoc = xbmcvfs.translatePath(utils.data_dir() + "mode.json")
     jsonObj = None
-    
+
     def __init__(self):
         #load the current mode
         self._openFile()
-    
+
     def run(self):
 
         currentMode = {
@@ -23,27 +23,27 @@ class Toggle:
             self.LOCAL_MODE: utils.getString(30012),
             self.UNKNOWN_MODE: utils.getString(30023)
         }
-        
-        if(xbmcgui.Dialog().yesno(utils.getString(30010), utils.getString(30024), currentMode[self.jsonObj['mode']], utils.getString(30025))):
-        
+
+        if(xbmcgui.Dialog().yesno(utils.getString(30010), utils.getString(30024) + " " + currentMode[self.jsonObj['mode']] + "\n" + utils.getString(30025))):
+
             mode = xbmcgui.Dialog().select(utils.getString(30010),[utils.getString(30011),utils.getString(30012)])
-            
+
             copyComplete = False
             if(mode == self.REMOTE_MODE):
                 copyComplete = self._copyFile(utils.getSetting('remote_filename'))
             elif(mode == self.LOCAL_MODE):
                 copyComplete = self._copyFile(utils.getSetting('local_filename'))
-    
-            
+
+
             if(copyComplete):
                 self.jsonObj['mode'] = mode
                 self._writeFile()
-                
+
                 #prompt the user to restart xbmc
-                restartXbmc = xbmcgui.Dialog().yesno(utils.getString(30010),"",utils.getString(30013))
-    
+                restartXbmc = xbmcgui.Dialog().yesno(utils.getString(30010), utils.getString(30013))
+
                 if(restartXbmc):
-                    
+
                     #on windows just restart the app
                     if(xbmc.getCondVisibility('System.Platform.Windows')):
                         xbmc.executebuiltin('RestartApp()')
@@ -61,35 +61,35 @@ class Toggle:
                 xbmcvfs.delete(advanced_settings)
 
             #copy the new file
-            xbmcvfs.copy(xbmc.translatePath(filename),advanced_settings)
-            
+            xbmcvfs.copy(xbmc.translatePath(filename), advanced_settings)
+
             return True
         else:
-            xbmcgui.Dialog().ok(utils.getString(30010),utils.getString(30014))
-            
+            xbmcgui.Dialog().ok(utils.getString(30010), utils.getString(30014))
+
             return False
-        
+
     def _openFile(self):
         if(xbmcvfs.exists(self.fileLoc)):
             f = xbmcvfs.File(self.fileLoc)
-            
+
             self.jsonObj = json.loads(f.read())
-            
+
             f.close()
-            
+
         else:
-            self.jsonObj = {"mode":self.UNKNOWN_MODE}
-            
+            self.jsonObj = {"mode": self.UNKNOWN_MODE}
+
     def _writeFile(self):
-        
+
         #make the data dir if it doesn't exist
         if(not xbmcvfs.exists(utils.data_dir())):
             xbmcvfs.mkdir(utils.data_dir())
-        
+
         f = xbmcvfs.File(self.fileLoc,'w')
-        
+
         f.write(json.dumps(self.jsonObj))
-        
+
         f.close()
 
 
--- resources/lib/utils.py
+++ resources/lib/utils.py
@@ -1,29 +1,43 @@
 import xbmc
 import xbmcaddon
+import xbmcgui
+import xbmcvfs
 
 __addon_id__= 'script.toggle.local'
 __Addon = xbmcaddon.Addon(__addon_id__)
 
+
 def data_dir():
     return __Addon.getAddonInfo('profile')
+
 
 def addon_dir():
     return __Addon.getAddonInfo('path')
 
-def log(message,loglevel=xbmc.LOGDEBUG):
-    xbmc.log(encode(__addon_id__ + ": " + message),level=loglevel)
+
+def log(message, loglevel=xbmc.LOGDEBUG):
+    xbmc.log(__addon_id__ + ": " + message, level=loglevel)
+
 
 def showNotification(message):
-    xbmc.executebuiltin("Notification(" + getString(30010) + "," + message + ",4000," + xbmc.translatePath(__Addon.getAddonInfo('path') + "/icon.png") + ")")
+    xbmcgui.Dialog().notification(getString(30010), message, time=5000, icon=xbmcvfs.translatePath(__Addon.getAddonInfo('path') + "/images/icon.png"), sound=False)
+
 
 def getSetting(name):
     return __Addon.getSetting(name)
 
-def setSetting(name,value):
+
+def setSetting(name, value):
     __Addon.setSetting(name,value)
-    
+
+
 def getString(string_id):
     return __Addon.getLocalizedString(string_id)
 
-def encode(string):
-    return string.encode('UTF-8','replace')
+
+def getSettingBool(name):
+    return bool(__Addon.getSettingBool(name))
+
+
+def getSettingInt(name):
+    return __Addon.getSettingInt(name)
